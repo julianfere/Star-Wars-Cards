60c85b9c985bcef95101ee6eed6b45290212bb61
Julian Feregotti <julian.feregotti@snappler.com>
2022-01-05 15:14:45 -0300

    added new fetch method for images

--- src/api/swapi.js
+++ src/api/swapi.js
@@ -1,5 +1,12 @@
-export const fetchApi = async (uri) => {
+export const fetchSwapi = async (uri) => {
   const res = await fetch(`https://swapi.dev/api/${uri}`);
   const data = await res.json();
   return data.results;
+}
+
+
+export const fetchApi = async (uri) => {
+  const res = await fetch(`https://akabab.github.io/starwars-api/api/${uri}`);
+  const data = await res.json();
+  return data;
 }
